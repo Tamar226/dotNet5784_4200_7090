d289cb4112c2ce1ad21007c83aa5c3e11ebfedaa
Tamar226 <tamarlicht3@gmail.com>
2023-10-24 20:24:50 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program7090.cs
@@ -0,0 +1,16 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome7090();
+        Welcome4200();
+        Console.ReadKey();
+    }
+    static partial void Welcome4200();
+    private static void Welcome7090()
+    {
+        Console.WriteLine("hellow my dear");
+        string massage = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console", massage);
+    }
+}
